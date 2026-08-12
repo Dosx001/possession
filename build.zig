56bcfb4f1501cbe3c84bb28c1623e85bca77e94e
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2026-08-12 12:15:51 -0700

    build: rename pkg to possession

--- build.zig
+++ build.zig
@@ -10,7 +10,7 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
     const exe = b.addExecutable(.{
-        .name = "scalpel",
+        .name = "mcjd",
         .root_module = exe_mod,
     });
     b.installArtifact(exe);
@@ -21,7 +21,7 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
     const client = b.addExecutable(.{
-        .name = "scalpel-client",
+        .name = "mcjc",
         .root_module = client_mod,
     });
     b.installArtifact(client);
